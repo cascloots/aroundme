d5b09f501d480b2aa6541322437e00cbc7f653ab
Cas Cloots <cloots.cas@gmail.com>
2022-07-09 20:41:32 +0200

    initial setup

---
+++ Shared/AroundMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundMeApp.swift
+//  Shared
+//
+//  Created by Cas Cloots on 09/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AroundMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
